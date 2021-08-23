16e07b55d7fb57c52e227b3aef9ed20eda10350a
vijaydvk <vijayakumard01@gmail.com>
2021-08-23 15:02:29 +0530

    uploaded new files

---
+++ actions.dart
@@ -0,0 +1,15 @@
+enum Action {
+  Increment, Decrement
+}
+
+class IncrementAction { // sealed
+  int count;
+
+  IncrementAction(this.count);
+}
+
+class DecrementAction {
+  int count;
+
+  DecrementAction(this.count);
+}
